cd29b15b1fd6eed66cb0af78067119098957fa53
Chloe Kwon <70490596+chloedkkwon@users.noreply.github.com>
2025-03-07 18:42:06 -0500

    Update write_grid.m

--- write_grid.m
+++ write_grid.m
@@ -1,11 +1,13 @@
+% Written by Chloe D. Kwon (chloe.dkkwon@gmail.com)
+% March 7, 2025
+% Creates a grid based on a matlab struct object
+
 function [] = write_grid(filename, T)
-    % Open file for writing
     fid = fopen(filename, 'w');
     if fid == -1
         error('Cannot open file for writing: %s', filename);
     end
     
-    % Write TextGrid header
     fprintf(fid, 'File type = "ooTextFile"\n');
     fprintf(fid, 'Object class = "TextGrid"\n\n');
     fprintf(fid, '%.6f\n%.6f\n', T(1).xmin, T(1).xmax);  % xmin and xmax
@@ -18,16 +20,14 @@ function [] = write_grid(filename, T)
         fprintf(fid, '"%s"\n', T(i).name);   % Tier name
         fprintf(fid, '%.6f\n%.6f\n', T(i).xmin, T(i).xmax); % xmin and xmax
         fprintf(fid, '%d\n', T(i).intervals);  % Number of intervals
-        
-        % Add placeholder intervals (adjust as needed)
+
         for j = 1:T(i).intervals
             xmin_interval = T(i).xmin + (j - 1) * (T(i).xmax - T(i).xmin) / T(i).intervals;
             xmax_interval = T(i).xmin + j * (T(i).xmax - T(i).xmin) / T(i).intervals;
             fprintf(fid, '%.6f\n%.6f\n""\n', xmin_interval, xmax_interval);  % Empty label
         end
     end
-    
-    % Close file
+
     fclose(fid);
     fprintf('TextGrid saved to %s\n', filename);
 end
